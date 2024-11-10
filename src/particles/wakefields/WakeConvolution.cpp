9b1e8bd0944f0b0f4bd0c2ef396300aee1b0f5ee
Weiqun Zhang <weiqunzhang@lbl.gov>
2024-11-10 11:11:11 -0800

    Use amrex FFT

--- src/particles/wakefields/WakeConvolution.cpp
+++ src/particles/wakefields/WakeConvolution.cpp
@@ -11,7 +11,7 @@
 #include "particles/ImpactXParticleContainer.H"
 
 #ifdef ImpactX_USE_FFT
-#include <ablastr/math/fft/AnyFFT.H>
+#include <AMReX_FFT.H>
 #endif
 
 #include <AMReX_REAL.H>
@@ -78,7 +78,7 @@ namespace impactx::particles::wakefields
         int const complex_size = padded_signal_size / 2 + 1;
 
         // Allocate memory for FFT inputs and outputs
-        using ablastr::math::anyfft::Complex;
+        using Complex = amrex::GpuComplex<amrex::Real>;
 
         // Signal padding for periodicity of FFTs:
         // - slope starts/end on zero, zero-pad the rest
@@ -102,19 +102,16 @@ namespace impactx::particles::wakefields
         // Define Forward FFT
         amrex::Gpu::DeviceVector<Complex> out1(complex_size);
         amrex::Gpu::DeviceVector<Complex> out2(complex_size);
-        // TODO: n does not change usually, so we can keep the plans alive over the simulation
-        //       runtime. To do that, we can make this function a functor class.
-        auto p1 = ablastr::math::anyfft::CreatePlan(
-                amrex::IntVect(padded_signal_size), in1.data(), out1.data(), ablastr::math::anyfft::direction::R2C, 1
-        );
-        auto p2 = ablastr::math::anyfft::CreatePlan(
-                amrex::IntVect(padded_signal_size), in2.data(), out2.data(), ablastr::math::anyfft::direction::R2C, 1
-        );
+
+        amrex::FFT::LocalR2C<amrex::Real,amrex::FFT::Direction::forward>
+            fft_p1{amrex::IntVect(padded_signal_size)};
+        amrex::FFT::LocalR2C<amrex::Real,amrex::FFT::Direction::forward>
+            fft_p2{amrex::IntVect(padded_signal_size)};
 
         // Perform Forward FFT - Convert inputs into frequency domain
         // Gives out1,out2, the FFT-transformed input arrays of in1, in2
-        ablastr::math::anyfft::Execute(p1);
-        ablastr::math::anyfft::Execute(p2);
+        fft_p1.forward(in1.data(), out1.data());
+        fft_p2.forward(in2.data(), out2.data());
 
         // Perform FFT Multiplication - FFT Element-wise multiplication in frequency space
         amrex::Gpu::DeviceVector<Complex> conv_result(complex_size);
@@ -123,21 +120,18 @@ namespace impactx::particles::wakefields
         Complex const * const dptr_out2 = out2.data();
         amrex::ParallelFor(complex_size, [=] AMREX_GPU_DEVICE (int i) noexcept
         {
-            using ablastr::math::anyfft::multiply;
-            multiply(dptr_conv_result[i], dptr_out1[i], dptr_out2[i]);
+            dptr_conv_result[i] = dptr_out1[i] * dptr_out2[i];
         });
 
         // Define Backward FFT - Revert from frequency domain to time/space domain
         amrex::Gpu::DeviceVector<amrex::Real> out3(padded_signal_size, 0.0);
-        // TODO: n does not change usually, so we can keep the plans alive over the simulation
-        //       runtime. To do that, we can make this function a functor class.
         amrex::Real * const dptr_out3 = out3.data();
-        auto p3 = ablastr::math::anyfft::CreatePlan(
-                amrex::IntVect(padded_signal_size), dptr_out3, dptr_conv_result, ablastr::math::anyfft::direction::C2R, 1
-        );
+
+        amrex::FFT::LocalR2C<amrex::Real,amrex::FFT::Direction::backward>
+            fft_p3{amrex::IntVect(padded_signal_size)};
 
         // Perform Backward FFT
-        ablastr::math::anyfft::Execute(p3);
+        fft_p3.backward(dptr_conv_result, dptr_out3);
 
         // Crop to first N values, normalize result by the output size and multiply result by bin size
         amrex::Gpu::DeviceVector<amrex::Real> result(beam_profile_slope_size, 0.0);
@@ -146,13 +140,6 @@ namespace impactx::particles::wakefields
         {
             dptr_result[i] = dptr_out3[i] / padded_signal_size * delta_t;  // TODO: padded_signal_size or beam_profile_slope_size (2N or N)?
         });
-
-        // Clean up intermediate declarations
-        // TODO: n does not change usually, so we can keep the plans alive over the simulation
-        //       runtime. To do that, we can make this function a functor class.
-        ablastr::math::anyfft::DestroyPlan(p1);
-        ablastr::math::anyfft::DestroyPlan(p2);
-        ablastr::math::anyfft::DestroyPlan(p3);
 
         return result;
 #else
